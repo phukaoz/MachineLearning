c251b326bdee579e8089a70caddabdd6f4666c79
phukaoz <tnpkofficial@gmail.com>
2024-01-28 18:43:14 +0700

    add some function for matrix

--- framework.c
+++ framework.c
@@ -2,7 +2,70 @@
 #include "framework.h"
 #include <time.h>
 
+typedef struct{
+    mat a0;
+    mat w1, b1, a1;
+    mat w2, b2, a2;
+}Xor;
+
+void forward_xor(Xor m){
+    matmul(m.a1, m.a0, m.w1);
+    matsum(m.a1, m.b1);
+    matsig(m.a1);
+
+    matmul(m.a2, m.a1, m.w2);
+    matsum(m.a2, m.b2);
+    matsig(m.a2);
+}
+
+float cost(Xor m, mat ti, mat to){
+    assert(ti.rows == to.rows);
+    assert(to.cols == m.a2.cols);
+    size_t n = ti.rows;
+    
+    float c = 0;
+    for (size_t i = 0; i < n; ++i){
+        mat x = matrow(ti, i);
+        mat y = matrow(to, i);
+        
+        matcpy(m.a0, x);
+        forward_xor(m);
+
+        size_t q = to.cols;
+        for (size_t j = 0; j < q; ++j){
+            float d = matat(m.a2,0,j) - matat(y, 0, j);
+            c += d*d;
+        }
+    }
+    return c/n;
+}
+
 int main(void){
+    srand(time(0));
+    Xor m;
+    m.a0 = matalloc(1,2);
+    m.w1 = matalloc(2,2);
+    m.b1 = matalloc(1,2);
+    m.a1 = matalloc(1,2);
+    m.w2 = matalloc(2,1);
+    m.b2 = matalloc(1,1);
+    m.a2 = matalloc(1,1);
+
+    matrand(m.w1, 0, 1);
+    matrand(m.b1, 0, 1);
+    matrand(m.w2, 0, 1);
+    matrand(m.b2, 0, 1);
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            matat(m.a0, 0, 0) = i;
+            matat(m.a0, 0, 1) = j;
+            forward_xor(m);
+            float y = *m.a2.es;
+
+            printf("%zu ^ %zu = %f\n", i, j, y);
+        }
+    }
 
     return 0;
 }
--- framework.h
+++ framework.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <math.h>
 
 #ifndef FRAMEWORK_MALLOC
 #include <stdlib.h>
@@ -17,19 +18,24 @@
 typedef struct {
     size_t rows;
     size_t cols;
+    size_t stride;
     float *es;
-} Mat;
+} mat;
 
-#define MAT_AT(m,i,j) (m).es[(i)*(m).cols + (j)]
+#define matat(m,i,j) (m).es[(i)*(m).stride + (j)]
 
 float rand_float(void);
+float sigmoid(float x);
 
-Mat mat_alloc(size_t rows, size_t cols);
-void mat_fill(Mat dest, float val);
-void mat_dot(Mat dest, Mat a, Mat b);
-void mat_sum(Mat dest, Mat a);
-void mat_print(Mat m);
-void mat_rand(Mat m, float low, float high);
+mat matalloc(size_t rows, size_t cols);
+void matfill(mat dest, float val);
+void matmul(mat dest, mat a, mat b);
+void matsum(mat dest, mat a);
+void matprint(mat m);
+void matrand(mat m, float low, float high);
+void matcpy(mat dest, mat src);
+void matsig(mat m);
+mat matrow(mat src, size_t i);
 
 #endif //FRAMEWORK_H_
 
@@ -39,24 +45,37 @@ float rand_float(void){
     return (float)rand() / (float)RAND_MAX;
 }
 
-Mat mat_alloc(size_t rows, size_t cols){
-    Mat ret;
+float sigmoid(float x){
+    return 1.f/(1.f + expf(-x));
+}
+
+mat matalloc(size_t rows, size_t cols){
+    mat ret;
     ret.rows = rows;
     ret.cols = cols;
+    ret.stride = cols;
     ret.es = FRAMEWORK_MALLOC(sizeof(*ret.es)*rows*cols);
     FRAMEWORK_ASSERT(ret.es != NULL);
     return ret;
 }
 
-void mat_fill(Mat dest,float val){
-    for(size_t i=0;i<dest.rows; i++){
-        for(size_t j=0;j<dest.cols; j++){
-            MAT_AT(dest,i,j) = val;
+void matsig(mat m){
+    for(size_t i = 0;i<m.rows;i++){
+        for(size_t j =0;j<m.cols;j++){
+            matat(m,i,j) = sigmoid(matat(m,i,j));
         }
     }
 }
 
-void mat_dot(Mat dest, Mat a, Mat b){
+void matfill(mat dest,float val){
+    for(size_t i=0;i<dest.rows; i++){
+        for(size_t j=0;j<dest.cols; j++){
+            matat(dest,i,j) = val;
+        }
+    }
+}
+
+void matmul(mat dest, mat a, mat b){
     FRAMEWORK_ASSERT(a.cols == b.rows);
     FRAMEWORK_ASSERT(dest.rows == a.rows);
     FRAMEWORK_ASSERT(dest.cols == b.cols);
@@ -64,35 +83,55 @@ void mat_dot(Mat dest, Mat a, Mat b){
     for(size_t i=0;i<a.rows;i++){
         for(size_t k=0;k<a.cols;k++){
             for(size_t j=0;j<b.cols;j++){
-                MAT_AT(dest,i,j) += MAT_AT(a,i,k) + MAT_AT(b,k,j);
+                matat(dest,i,j) += matat(a,i,k) + matat(b,k,j);
             }
         }
     }
 }
 
-void mat_sum(Mat dest, Mat a){
-    FRAMEWORK_ASSERT(dest.rows == a.rows);
-    FRAMEWORK_ASSERT(dest.cols == a.cols);
-    for(size_t i = 0; i < dest.rows; i++){
-        for(size_t j=0; j < dest.cols; j++){
-            MAT_AT(dest,i,j) += MAT_AT(a,i,j);
+void matcpy(mat dest, mat src){
+    FRAMEWORK_ASSERT(dest.cols == src.cols);
+    FRAMEWORK_ASSERT(dest.rows == src.rows);
+    for(size_t i = 0; i < dest.rows;i++){
+        for(size_t j=0;j < dest.cols;j++){
+            matat(dest,i,j) = matat(src,i,j);
         }
     }
 }
 
-void mat_print(Mat m){
+void matsum(mat dest, mat a){
+    FRAMEWORK_ASSERT(dest.rows == a.rows);
+    FRAMEWORK_ASSERT(dest.cols == a.cols);
+    for(size_t i = 0; i < dest.rows; i++){
+        for(size_t j=0; j < dest.cols; j++){
+            matat(dest,i,j) += matat(a,i,j);
+        }
+    }
+}
+
+mat matrow(mat src, size_t row){
+    FRAMEWORK_ASSERT(row < src.rows);
+    return (mat){
+        .rows = 1,
+        .cols = src.cols,
+        .stride = src.stride,
+        .es = &matat(src, row, 0),
+    };
+}
+
+void matprint(mat m){
     for(size_t i = 0; i < m.rows; i++){
         for(size_t j=0; j < m.cols; j++){
-            printf("%f ",MAT_AT(m,i,j));
+            printf("%f ",matat(m,i,j));
         }
         printf("\n");
     }
 }
 
-void mat_rand(Mat m, float low, float high){
+void matrand(mat m, float low, float high){
     for(size_t i = 0; i < m.rows; i++){
         for(size_t j=0; j < m.cols; j++){
-            MAT_AT(m,i,j) = rand_float()*(high-low) +low;
+            matat(m,i,j) = rand_float()*(high-low) +low;
         }
     }
 }
