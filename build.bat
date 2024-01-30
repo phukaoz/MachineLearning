cc38402117c3f32c96121ed2c2e1e99b4d2f8051
phukaoz <tnpkofficial@gmail.com>
2024-01-30 07:27:27 +0700

    drop: build.bat
    use makefile instead.

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-gcc -g -c framework/matrix.c -o framework/matrix.o -lm
-gcc -g -c main.c -o main.o -lm
-gcc -g framework/matrix.o main.o -o main -lm
-.\main
-del main.o & del framework\matrix.o
